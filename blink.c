745cbd5eee2ecf111b286ab63825860daa8d81bd
NicolasCheshire <nicolasramir@gmail.com>
2021-01-04 14:53:17 +0100

    Update blink.c
    include button

--- blink.c
+++ blink.c
@@ -1,15 +1,21 @@
 #include <wiringPi.h>
 
 const int ledpin = 17;
+const int buttonpin = 15;
 
 int main(void){
 	wiringPiSetupGpio();
 	pinMode(ledpin, OUTPUT);
+	pinMode(buttonpin, INPUT);
+	pullUpDnControl(15, PUD_DOWN);
 
 	while(1){
-		digitalWrite(ledpin, HIGH);
-		delay(500);
-		digitalWrite(ledpin, LOW);
-		delay(500);
+		if(digitalRead(buttonpin)){
+			digitalWrite(ledpin, HIGH);
+		}
+		else{
+			digitalWrite(ledpin, LOW);
+		}
+		
 	}
 }
